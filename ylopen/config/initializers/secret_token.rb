40abe5ba33ce2014e0e03617b1ab3a1fbebd1e53
kelvinlong <longkai@gmail.com>
2012-08-24 17:21:31 +0800

    import ylopen

---
+++ ylopen/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ylopen::Application.config.secret_token = 'aa1464e82a7bde7dc88deaa65f6e34520e1a11172b46deb381e29f26f94a5e1e495565b4d22e98d13eb1396c3d57c14dddbf4df9c271430732c15868daab5865'
